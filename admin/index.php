6b8162886747432de90cc135a2488deaba329847
hernan <hernan.laurel@gmail.com>
2023-11-29 18:06:11 -0300

    terer commit 29-11-23 	modified:   admin/index.php 	new file:   admin/styles.css 	new file:   admin/uploads/img_6567a77b2d388.jpg 	new file:   admin/uploads/img_6567a786d4193.jpg 	new file:   admin/uploads/img_6567a7a2434b4.jpg

--- admin/index.php
+++ admin/index.php
@@ -11,14 +11,13 @@ $videos = get_vids();
 
 <html>
 <head>
-
   <title>publicidad Dreams Coyhaique</title>
   <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
-  
-<link rel="shortcut icon" href="slotmachine.ico" />
+  <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Asegúrate de tener un archivo de estilos CSS -->
+  <link rel="shortcut icon" href="slotmachine.ico" />
 </head>
 <body>
-<?php include("navbar.php");?>
+  <?php include("navbar.php");?>
   <div class="container">
     <div class="row">
       <div class="col-md-12">
@@ -26,45 +25,35 @@ $videos = get_vids();
         <a href="./form_image.php" class="btn btn-default">Agregar imagen</a> 
         <a href="./form_video.php" class="btn btn-default">Agregar video</a> 
         <br><br>
-        <?php if (count($images) > 0 || count($videos) > 0): ?>
-        <table class="table table-bordered">
-          <thead>
-            <th>Archivo</th>
-            <th>Tipo</th>
-            <th>Fecha de termino</th>
-            <th>Acciones</th>
-          </thead>
-          <?php foreach($images as $img): ?>
-          <tr>
-            <td><img src="<?php echo $img->folder.$img->src; ?>" style="width:180px;"></td>
-            <td>Imagen</td>
-            <td><?php echo $img->fecha; ?></td>
-            <td>
-              <a class="btn btn-success" href="./download.php?id=<?php echo $img->id; ?>">Descargar</a> 
-              <a class="btn btn-danger" href="./delete.php?id=<?php echo $img->id; ?>">Eliminar</a>
-            </td>
-          </tr>
-          <?php endforeach; ?>
-          <?php foreach($videos as $vid): ?>
-          <tr>
-            <td>
-              <video controls width="180">
-                <source src="<?php echo $vid->folder.$vid->src; ?>" >
-                Tu navegador no soporta el elemento de video.
-              </video>
-            </td>
-            <td>Video</td>
-            <td><?php echo $vid->fecha; ?></td>
-            <td>
-              <a class="btn btn-success" href="./download.php?id=<?php echo $vid->id; ?>">Descargar</a> 
-              <a class="btn btn-danger" href="./delete.php?id=<?php echo $vid->id; ?>">Eliminar</a>
-            </td>
-          </tr>
-          <?php endforeach; ?>
-        </table>
-        <?php else: ?>
-        <h4 class="alert alert-warning">No hay imágenes ni videos.</h4>
-        <?php endif; ?>
+        <div class="image-grid">
+          <?php if (count($images) > 0 || count($videos) > 0): ?>
+            <?php foreach($images as $img): ?>
+              <div class="grid-item">
+                <img src="<?php echo $img->folder.$img->src; ?>" style="width:100%;">
+                <p><?php echo $img->fecha; ?></p>
+                <div>
+                  <a class="btn btn-success" href="./download.php?id=<?php echo $img->id; ?>">Descargar</a> 
+                  <a class="btn btn-danger" href="./delete.php?id=<?php echo $img->id; ?>">Eliminar</a>
+                </div>
+              </div>
+            <?php endforeach; ?>
+            <?php foreach($videos as $vid): ?>
+              <div class="grid-item">
+                <video controls width="100%">
+                  <source src="<?php echo $vid->folder.$vid->src; ?>" >
+                  Tu navegador no soporta el elemento de video.
+                </video>
+                <p><?php echo $vid->fecha; ?></p>
+                <div>
+                  <a class="btn btn-success" href="./download.php?id=<?php echo $vid->id; ?>">Descargar</a> 
+                  <a class="btn btn-danger" href="./delete.php?id=<?php echo $vid->id; ?>">Eliminar</a>
+                </div>
+              </div>
+            <?php endforeach; ?>
+          <?php else: ?>
+            <h4 class="alert alert-warning">No hay imágenes ni videos.</h4>
+          <?php endif; ?>
+        </div>
       </div>
     </div>
   </div>
