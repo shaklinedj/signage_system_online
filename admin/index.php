bfd5c6ba13e65f6d9419d44fdf3a356ae971185d
hernan <hernan.aurel@gmail.com>
2024-08-27 14:22:53 -0400

    reparado

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,8 @@ $videos = get_vids($casino_id);
 <head>
   <title>Publicidad Dreams Coyhaique</title>
   <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Asegúrate de tener un archivo de estilos CSS -->
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" href="../sweetalert2/sweetalert2.min.css">
   <link rel="shortcut icon" href="slotmachine.ico" />
   
   <script src="../sweetalert2/sweetalert2.min.js"></script>
@@ -37,26 +38,26 @@ $videos = get_vids($casino_id);
         <br><br>
         <div class="image-grid">
           <?php if (count($images) > 0 || count($videos) > 0): ?>
-            <?php foreach($images as $img): ?>
-              <div class="grid-item">
-                <img src="<?php echo $img->folder.$img->src; ?>" style="width:100%;">
+            <?php foreach ($images as $img): ?>
+              <div id="item-<?php echo $img->id; ?>" class="grid-item">
+                <img src="<?php echo $img->folder . $img->src; ?>" style="width:100%;" loading="lazy">
                 <p><?php echo $img->fecha; ?></p>
                 <div>
-                  <a class="btn btn-success" href="./download.php?id=<?php echo $img->id; ?>">Descargar</a> 
-                  <a class="btn btn-danger" href="./delete.php?id=<?php echo $img->id; ?>">Eliminar</a>
+                  <a class="btn btn-success" href="./download.php?id=<?php echo $img->id; ?>">Descargar</a>
+                  <a class="btn btn-danger delete-btn" data-id="<?php echo $img->id; ?>" href="#">Eliminar</a>
                 </div>
               </div>
             <?php endforeach; ?>
-            <?php foreach($videos as $vid): ?>
-              <div class="grid-item">
-                <video controls width="100%">
-                  <source src="<?php echo $vid->folder.$vid->src; ?>" >
+            <?php foreach ($videos as $vid): ?>
+              <div id="item-<?php echo $vid->id ?>" class="grid-item">
+                <video controls width="100%" preload="none" loading="lazy">
+                  <source src="<?php echo $vid->folder . $vid->src; ?>">
                   Tu navegador no soporta el elemento de video.
                 </video>
                 <p><?php echo $vid->fecha; ?></p>
                 <div>
-                  <a class="btn btn-success" href="./download.php?id=<?php echo $vid->id; ?>">Descargar</a> 
-                  <a class="btn btn-danger" href="./delete.php?id=<?php echo $vid->id; ?>">Eliminar</a>
+                  <a class="btn btn-success" href="./download.php?id=<?php echo $vid->id; ?>">Descargar</a>
+                  <a class="btn btn-danger delete-btn" data-id="<?php echo $vid->id; ?>" href="#">Eliminar</a>
                 </div>
               </div>
             <?php endforeach; ?>
